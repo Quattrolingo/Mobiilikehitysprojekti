33eaf79013d5d052a472139ddd39e50ab969d85f
Antti Moilanen <t0moan02@students.oamk.fi>
2022-11-10 18:14:02 +0200

    edited navbar

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,28 +1,73 @@
 import { StyleSheet, View, Image, Pressable } from 'react-native'
 
-export default function NavigationBar({view}) {
+export default function NavigationBar({setView, view}) {
   
     return (
       <View style={NavBarStyles.container}>
-        <Pressable onPress={() => view("ExerciseScrollView")}>
-          <Image
-            style={NavBarStyles.navbarBtn}
-            source={require('./../data/images/navigation_bar_home.png')} />
+        <Pressable onPress={() => setView("ExerciseScrollView")}>
+          {
+            (view == "ExerciseScrollView") ?
+            <View style={NavBarStyles.navbarBtnActive}>
+              <Image   
+              style={NavBarStyles.navbarBtn}           
+              source={require('./../data/images/navigation_bar_home.png')} />
+            </View>
+            :
+            <View style={NavBarStyles.navbarBtnInactive}>
+              <Image   
+              style={NavBarStyles.navbarBtn}           
+              source={require('./../data/images/navigation_bar_home.png')} />
+            </View>
+          }          
         </Pressable>
-        <Pressable onPress={() => view("Achievements")}>
-          <Image
-            style={NavBarStyles.navbarBtn}
-            source={require('./../data/images/navigation_bar_achievements.png')} />
+        <Pressable onPress={() => setView("Achievements")}>
+          {
+            (view == "Achievements") ?
+            <View style={NavBarStyles.navbarBtnActive}>
+              <Image
+              style={NavBarStyles.navbarBtn}
+              source={require('./../data/images/navigation_bar_achievements.png')} />
+            </View>
+            :
+            <View style={NavBarStyles.navbarBtnInactive}>
+              <Image
+              style={NavBarStyles.navbarBtn}
+              source={require('./../data/images/navigation_bar_achievements.png')} />
+            </View>
+          }          
         </Pressable>
-        <Pressable onPress={() => view("Profile")}>
-          <Image
-            style={NavBarStyles.navbarBtn}
-            source={require('./../data/images/navigation_bar_profile.png')} />
+        <Pressable onPress={() => setView("Profile")}>
+          {
+            (view == "Profile") ?
+            <View style={NavBarStyles.navbarBtnActive}>
+              <Image
+              style={NavBarStyles.navbarBtn}
+              source={require('./../data/images/navigation_bar_profile.png')} />
+            </View>
+            :
+            <View style={NavBarStyles.navbarBtnInactive}>
+              <Image
+              style={NavBarStyles.navbarBtn}
+              source={require('./../data/images/navigation_bar_profile.png')} />
+            </View>
+          }          
         </Pressable >
-        <Pressable onPress={() => view("Dictionary")}>
-          <Image
-            style={NavBarStyles.navbarBtn}
-            source={require('./../data/images/navigation_bar_dictionary.png')} />
+        <Pressable onPress={() => setView("Dictionary")}>
+          {
+            (view == "Dictionary") ?
+            <View style={NavBarStyles.navbarBtnActive}>
+              <Image
+              style={NavBarStyles.navbarBtn}
+              source={require('./../data/images/navigation_bar_dictionary.png')} />
+            </View>
+            :
+            <View style={NavBarStyles.navbarBtnInactive}>
+              <Image
+              style={NavBarStyles.navbarBtn}
+              source={require('./../data/images/navigation_bar_dictionary.png')} />
+            </View>
+          }
+          
         </Pressable>
       </View>
     );
@@ -41,6 +86,19 @@ export default function NavigationBar({view}) {
     },
     navbarBtn: {
       height: 35,
-      width: 35
+      width: 35,
+    },
+    navbarBtnActive: {
+      backgroundColor: '#f5e6c4',
+      padding: 5,
+      borderRadius: 15,
+      borderWidth: 1,
+      borderColor: '#fad47d' 
+    },
+    navbarBtnInactive: {
+      backgroundColor: 'white',
+      padding: 5,
+      borderWidth: 1,
+      borderColor: 'white' 
     }
   });
