9a10a0895e33af644b7e513e212f189d9446d975
Karahi1 <t0raka02@students.oamk.fi>
2022-11-13 14:55:06 +0200

    Random word working + styling

--- components/dictionary/DictStyles.js
+++ components/dictionary/DictStyles.js
@@ -1,11 +1,90 @@
 import { StyleSheet } from "react-native";
 
+
 export default StyleSheet.create({
-    container: {
+    dictContainer: {
       flex: 1,
       width: "100%",
+      backgroundColor: 'orange',   
+      alignItems: 'center',
+      
+    },
+    headline: {
+      marginTop: 20,
+      fontSize: 40,
+      fontWeight: 'bold',
+      fontFamily: 'monospace',
+      backgroundColor: "darkorange",
+    },
+    DictText: {
+      fontFamily: 'monospace',
+      fontSize: 15,
+      fontWeight: 'bold',
+      padding: 15,
+      marginHorizontal: 10,
+      textAlign: 'center',
+    },
+    random: {
+      marginTop: 50,
+      fontSize: 20,
+      fontWeight: 'bold',
+      fontFamily: 'monospace',
+      textDecorationLine: "underline",
+    },
+    BtnContainer: {
+      width: "60%",
+      height: "10%",
+      backgroundColor: 'black',
+      alignItems: "center",
+      justifyContent: "space-around",
+    },
+
+
+    container: {
+      marginTop: 20,
       backgroundColor: 'orange',
       alignItems: 'center',
       justifyContent: 'center',
+      
     },
-  });
+    mainContainer: {
+      flex: 1,
+      justifyContent: 'space-between',
+      padding: 20,
+      margin: 10,
+    },
+    rndContainer: {
+      flex: 0.35,
+      backgroundColor: 'beige',
+      borderWidth: 5,
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+      alignItems: 'center',
+    },
+    modal: {
+      marginTop: 100,
+      padding: 20,
+      backgroundColor: '#fafafa',
+      justifyContent: 'center',
+      alignItems: 'center',
+      shadowColor: 'red',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      }
+    },
+    close: {
+      marginTop: 50,
+      color: '#333',
+      fontWeight: 'bold',
+    },
+    push: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      textShadowColor: 'indianred',
+      textShadowOffset: {width: 5, height: 5},
+      textShadowRadius: 5,
+     
+    }
+
+});
--- components/dictionary/Dictionary.js
+++ components/dictionary/Dictionary.js
@@ -1,8 +1,18 @@
-import { Text, View } from 'react-native'
+import { Button, Text, Pressable, View, Modal } from 'react-native'
 import StyleSheet from './DictStyles';
 import { API_KEY } from '@env';
+import { useEffect, useState } from 'react';
+
 
 export default function Dictionary() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [word, setWord] = useState('');
+  const [definition, setDefinition] = useState('');
+
+  function close() {
+    setModalVisible(false);
+  }
 
   const options = {
     method: 'GET',
@@ -12,16 +22,65 @@ export default function Dictionary() {
     }
   };
   
+/*
   fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
+    .then(res => res.json())
+    .then(res => console.log(res))
     .catch(err => console.error(err));
+    */
+
+    useEffect(() => {
+    fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setWord(result.word);
+        setDefinition(result.results[0].definition);
+      },
+      (error) => {
+        alert(error);
+      }
+    )
+    }, [])
+
+
   
     return (
-      <View style={StyleSheet.container}>
-        <Text>This is DICTIONARY</Text>
+    <View style={StyleSheet.dictContainer}>
+      <Text style={StyleSheet.headline}>Opi lisää sanastoa</Text>
+      <View style={StyleSheet.mainContainer}>
+        <View style={StyleSheet.rndContainer}>
+        <Text style={StyleSheet.random}>Satunnainen sana</Text>
+        <Text style={StyleSheet.DictText}>Opi lisää opiskeltavan kielen sanoja ja niiden merkityksiä</Text>
+
+        <View style={StyleSheet.container}>
+          <Modal
+            visible={modalVisible}
+            onRequestClose={close}
+          >
+            <View style={StyleSheet.modal}>
+              <Text>Satunnainen sana</Text>
+              <Text>{word}</Text>
+              <Text>Sanan määritelmä</Text>
+              <Text>{definition}</Text>
+              <Pressable onPress={() => {
+                setModalVisible(false);
+              }}>
+                <Text style={StyleSheet.close}>Sulje</Text>
+              </Pressable>
+            </View>
+          </Modal>
+
+          <Pressable onPress={() => {
+            setModalVisible(true);
+          }}>
+            <Text style={StyleSheet.push}>PAINA TÄSTÄ SANA</Text>
+          </Pressable>
+        </View>
+        </View>
       </View>
-    );
-  }
-  
+    </View>
+  );
+}
+
   
